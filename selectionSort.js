f9b68e5340ec457c52e199611d2112dabe73476c
cambo2015 <cameronhansen91@outlook.com>
2021-07-30 11:58:41 -0600

    comments added

--- selectionSort.js
+++ selectionSort.js
@@ -3,14 +3,14 @@ const selectionSort = (myArr) => {
     const arr = [...myArr]
     let min = 0
     for (let i = 0; i < arr.length-2; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
+        for (let j = i + 1; j < arr.length; j++) { //set to the next element above i
             if (arr[j] < arr[min]) {
                 min = j //set curr min to current element if it is < the current min
            }
         }
-        if (min !== i) { //if min is not itself then swap the two elements (double check)
+        if (min !== i) { //if min is not itself then swap the two elements 
             [arr[i], arr[min]] = [arr[min], arr[i]]
-            console.log(arr)
+            // console.log(arr)
       }
     }
    return arr
