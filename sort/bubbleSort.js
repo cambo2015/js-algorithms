a42b463bb8ff5d441c2a4ff7f95606b8a7d6e805
cambo2015 <cameronhansen91@outlook.com>
2021-08-27 15:41:04 -0600

    added insertion sort and fixed insertion sort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -1,27 +1,25 @@
-
 /**
- * 
- * @param {Array} arr 
+ *
+ * @param {Array} arr
  * @returns {Array} sorted array
- * @description O(n^2) Bubble sort teaches us how to do check/compare two adjacent elements. It also teaches us how to swap  
+ * @description O(n^2) Bubble sort teaches us how to do check/compare two adjacent elements. It also teaches us how to swap
  */
 const bubleSort = (arr) => {
-    //https://www.youtube.com/watch?v=xli_FI7CuzA
+	//https://www.youtube.com/watch?v=xli_FI7CuzA
 
-    const temp = [...arr]
-    if (Array.isArray(temp)) {
-      for (let i = 0; i < temp.length; i++) {
-          
-          for (let j = 0; j < temp.length; j++) {
-             
-              if (temp[j] > temp[j + 1]) { //compare two adjacent items | if the left item is grater then swap them
-                  [temp[j],temp[j+1]] = [temp[j+1],temp[j]] //this is how you swap in js! It's a lot like python
-              }
-          }
-        }
-        // console.log(temp)
-       return temp
-    }
+	const temp = [...arr]
+	if (Array.isArray(temp)) {
+		for (let i = 0; i < temp.length; i++) {
+			for (let j = 0; j < temp.length; j++) {
+				if (temp[j] > temp[j + 1]) {
+					//compare two adjacent items | if the left item is grater then swap them
+					[temp[j], temp[j + 1]] = [temp[j + 1], temp[j]] //this is how you swap in js! It's a lot like python
+				}
+			}
+		}
+		// console.log(temp)
+		return temp
+	}
 }
 
 const bs = bubleSort(items)
---
+++ sort/insertionSort.js
@@ -0,0 +1,39 @@
+//insertionSort 
+/**
+ * @description Sorting algorithm . Also teaches how to go backwards in an array with a starting point
+ * @param arr 
+ */
+ const insertionSort = (arr) => {
+ 
+    for (let i = 1; i < arr.length; i++) { //0 wont run anyway in the while loop so skip it
+        let j = i  //this is the starting point. From here we will go backwards
+        while (j > 0 && arr[j - 1] > arr[j]) {  //nothing below zero. We compare the two arr[j-1] and arr[j] 
+          
+            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]] //if lowest one is higher swap
+            j-- //continue checking the numbers below
+        }
+        
+    }
+    return arr
+}
+
+const temp = insertionSort([5, 4, 6, 2, 1])
+
+
+
+console.log("")
+console.log("go backwards")
+const goBackwards = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        let startingPoint = i
+        while (startingPoint > 0) {
+            console.log(startingPoint)
+            console.log(arr[startingPoint])
+            startingPoint --
+        }
+    }
+}
+
+
+
+goBackwards([5,2,6,5])
